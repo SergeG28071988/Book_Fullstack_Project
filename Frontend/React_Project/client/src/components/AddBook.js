39e54526fb0d80fa051a015fe0ff14dad45839ad
SergeG28071988 <sergeygalckin2012@yandex.ru>
2024-09-26 16:17:41 +0300

    Реализовали стили, вывод и добавление книг. Протестировали приложение.

--- Frontend/React_Project/client/src/components/AddBook.js
+++ Frontend/React_Project/client/src/components/AddBook.js
@@ -1,9 +1,122 @@
-import React from 'react';
+import axios from 'axios';
+import React, {useState, useEffect} from 'react'
+import { useHistory } from 'react-router';
+import { useNavigate } from 'react-router-dom';
 
 const AddBook = () => {
+
+    const genres = [
+        'Триллер',
+        'Детектив',
+        'Мистика',
+        'Ужасы',
+        'Фантастика',       
+    ]; 
+
+    const [photo, setPhoto] = useState(null); // Изначально пустая строка
+    const [name, setName] = useState(''); // Изначально пустая строка
+    const [description, setDescription] = useState(''); // Изначально пустая строка    
+    const [genre, setGenre] = useState(''); // Изначально пустая строка
+    const [price, setPrice] = useState(''); // Изначально пустая строка
+    const [author, setAuthor] = useState(''); // Изначально пустая строка   
+
+    const navigate = useNavigate();
+
+    const AddBookInfo = async () => {
+        let formfield = new FormData()       
+        
+        formfield.append('name', name)
+        formfield.append('description', description)
+        formfield.append('genre', genre)
+        formfield.append('price', price)
+        formfield.append('author', author)       
+
+        if (photo != null){
+            formfield.append('photo', photo)
+        }
+
+        try {
+            const response = await axios.post('http://localhost:8000/api/books/', formfield);
+            console.log(response.data);
+            // Add your logic to handle successful order creation
+            // For example, you can redirect the user or show a success message
+            navigate('/');
+        } catch (error) {
+            console.error('Error adding order:', error);
+            // Handle the error (e.g., display an error message to the user)
+        }
+    }
+
     return (
         <div>
-            <h1>Add Book</h1>
+            <h1>Добавить книгу</h1>
+            <div className='container'>
+                <div className='form-group'>
+                    <div className='form-control'>     
+                        <label>Выберите фото книги</label>                  
+                        <input 
+                            type='file' 
+                            className='form-control form-control-lg'                             
+                            name='photo'
+                            onChange={(e) => setPhoto(e.target.files[0])}
+                        />
+                    </div>
+                    <div className='form-control'>
+                        <input 
+                            type='text' 
+                            className='form-control form-control-lg' 
+                            placeholder='Введите название книги'
+                            name='name' value={name}
+                            onChange={(e) => setName(e.target.value)}
+                        />
+                    </div>
+                    <div className='form-control'>
+                        <input 
+                            type='text' 
+                            className='form-control form-control-lg' 
+                            placeholder='Введите описание'
+                            name='description' value={description}
+                            onChange={(e) => setDescription(e.target.value)}
+                        />
+                    </div>                    
+                    <div className='form-control'>
+                        <select 
+                            className='form-control form-control-lg'
+                            value={genre}
+                            onChange={(e) => setGenre(e.target.value)}
+                        >
+                            <option value="">Выберите жанр</option> 
+                            {genres.map((genre) => (
+                            <option key={genre} value={genre}>
+                                {genre}
+                            </option>
+                            ))}
+                        </select>
+                    </div>                   
+                    <div className='form-control'>
+                        <input
+                            type='number' 
+                            className='form-control form-control-lg' 
+                            placeholder='Цена'
+                            name='price'
+                            value={price}
+                            onChange={(e) => setPrice(e.target.value)} 
+                        />
+                    </div>    
+                    <div className='form-control'>
+                        <input 
+                            type='text' 
+                            className='form-control form-control-lg' 
+                            placeholder='Введите автора'
+                            name='author' value={author}
+                            onChange={(e) => setAuthor(e.target.value)}
+                        />
+                    </div>                     
+                    
+                    <button className='btn btn-primary' type="submit" 
+                        onClick={AddBookInfo}>Добавить книгу</button>
+                </div>
+            </div>
         </div>
     );
 }
--- Frontend/React_Project/client/src/components/ShowBooks.js
+++ Frontend/React_Project/client/src/components/ShowBooks.js
@@ -1,9 +1,81 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react'
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 const ShowBooks = () => {
+
+    const [books, setBooks] = useState([])
+
+    const [searchQuery, setSearchQuery] = useState(''); // Состояние для поиска
+
+    const getBooks = async () => {
+        const response = await axios.get('http://localhost:8000/api/books/') 
+        // console.log(response.data)
+        setBooks(response.data)
+    }
+
+    useEffect(() => {
+        getBooks();
+    }, [])
+
+    const handleSearchChange = (event) => {
+        setSearchQuery(event.target.value);
+    };
+
+    const filteredBooks = books.filter((book) => {
+        return book.name.toString().includes(searchQuery); 
+    });
+
+    const handlePrint = () => {
+        window.print();
+    };
+
     return (
         <div>
-            <h1>Show All The Books</h1>
+            <div class="row text-center text-dark lh-2">
+                 <h4>Список книг в БД</h4>
+                 <div className="col-md-4 offset-md-4"> 
+                    <input 
+                        type="text" 
+                        className="form-control" 
+                        placeholder="Поиск по названию книги" 
+                        value={searchQuery} 
+                        onChange={handleSearchChange} 
+                    />
+                </div>
+            </div>
+            <br />
+            <button className="btn btn-secondary" onClick={handlePrint}>Печать</button> <br /><br/>
+            <table className="table table-hover table-striped table-bordered text-start">
+                <thead>
+                    <tr>
+                        <th>Фото</th>
+                        <th>Название</th>
+                        <th>Описание</th>
+                        <th>Жанр</th>
+                        <th>Цена</th>
+                        <th>Автор</th>                    
+                        <th>Детали</th>
+                    </tr>
+                </thead>
+                <tbody>
+                       {filteredBooks.map((book, index) => (
+                        <tr key={index}> 
+                            <td><img src={book.photo} alt="Book Photo" style={{ maxWidth: '100px' }} /></td> 
+                            <td>{book.name}</td>
+                            <td>{book.description}</td>
+                            <td>{book.genre}</td>
+                            <td>{book.price}</td>
+                            <td>{book.author}</td>                           
+                            <td> 
+                                <Link to={`/book/${book.id}`}> 
+                                    <button className="btn btn-primary btn-sm">Детали</button> 
+                                </Link>
+                            </td> 
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </div>
     );
 }
